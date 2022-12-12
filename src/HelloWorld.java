6034a1b5304993e215ab00dcb7ab5cf9c455d91a
Awal Kautsar <Awallkautsar@gmail.com>
2022-12-12 21:36:31 +0700

    added TextVar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 import static java.lang.System.*;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         out.println("Hello Backend");
         out.println("I'm Coming");
